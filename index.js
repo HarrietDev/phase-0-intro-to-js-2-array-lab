83c0130ba102ad14ccd5d1f524704361cc9fcc40
Harriet Chepngeno <harrietseroney2023@gmail.com>
2025-03-18 01:05:29 +0300

    arrays lab test done

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-// Write your solution here!
+const cats = ["Milo","Otis","Garfield"];
+
+function destructivelyAppendCat(){
+    cats.push("Ralph");
+}
+function destructivelyPrependCat(){
+    cats.unshift("Bob");
+}
+function destructivelyRemoveLastCat(){
+    cats.pop("Ralph");
+}
+function destructivelyRemoveFirstCat(){
+    cats.shift("Bob");
+}
+function appendCat(){
+  const allCats=[...cats, "Broom"];
+  return allCats;
+}
+function prependCat(){
+    const myCats = ["Arnold", ...cats];
+    return myCats;
+}
+function removeLastCat(){
+    const copyOfcats =cats.slice();
+    copyOfcats.pop();
+    return copyOfcats;
+}
+function removeFirstCat(){
+    const myFavCats=cats.slice();
+    myFavCats.shift();
+    return myFavCats;
+}
